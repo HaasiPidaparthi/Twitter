07ffff7953840f0d622bb15796803586a23d2c47
Haasitha_Pidaparthi <haasitha123@gmail.com>
2020-09-26 23:51:48 -0700

    twitter part 2

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -15,6 +15,13 @@ class HomeTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
+        loadTweets()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         loadTweets()
     }
     
@@ -44,6 +51,9 @@ class HomeTableViewController: UITableViewController {
         
         let user =  tweetArray[indexPath.row]["user"] as! NSDictionary
         
+        let date = tweetArray[indexPath.row]["created_at"] as? String
+        cell.tweetDate.text = getDate(dateString: date!)!.timeAgoSinceDate()
+        
         cell.userNameLabel.text = user["name"] as? String
         cell.tweetContent.text = tweetArray[indexPath.row]["text"] as? String
          
@@ -54,7 +64,19 @@ class HomeTableViewController: UITableViewController {
             cell.profileImageView.image = UIImage(data: imageData)
         }
         
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
+        
         return cell
+    }
+    
+    func getDate(dateString: String) -> Date? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "E MMM d HH:mm:ss Z yyyy"
+        dateFormatter.timeZone = TimeZone.current
+        dateFormatter.locale = Locale.current
+        return dateFormatter.date(from: dateString) // replace Date String
     }
     
     
@@ -70,5 +92,35 @@ class HomeTableViewController: UITableViewController {
         return tweetArray.count
     }
 
+}
 
+extension Date {
+    func timeAgoSinceDate() -> String {
+        // From Time
+        let fromDate = self
+        // To Time
+        let toDate = Date()
+        // Estimation
+        // Year
+        if let interval = Calendar.current.dateComponents([.year], from: fromDate, to: toDate).year, interval > 0  {
+            return interval == 1 ? "\(interval)" + " " + "y ago" : "\(interval)" + " " + "y ago"
+        }
+        // Month
+        if let interval = Calendar.current.dateComponents([.month], from: fromDate, to: toDate).month, interval > 0  {
+            return interval == 1 ? "\(interval)" + " " + "m ago" : "\(interval)" + " " + "m ago"
+        }
+        // Day
+        if let interval = Calendar.current.dateComponents([.day], from: fromDate, to: toDate).day, interval > 0  {
+            return interval == 1 ? "\(interval)" + " " + "d ago" : "\(interval)" + " " + "d ago"
+        }
+        // Hours
+        if let interval = Calendar.current.dateComponents([.hour], from: fromDate, to: toDate).hour, interval > 0 {
+            return interval == 1 ? "\(interval)" + " " + "h ago" : "\(interval)" + " " + "h ago"
+        }
+        // Minute
+        if let interval = Calendar.current.dateComponents([.minute], from: fromDate, to: toDate).minute, interval > 0 {
+            return interval == 1 ? "\(interval)" + " " + "min ago" : "\(interval)" + " " + "min ago"
+        }
+        return "a moment ago"
+    }
 }
--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -14,6 +14,58 @@ class TweetCellTableViewCell: UITableViewCell {
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var tweetContent: UILabel!
     
+    @IBOutlet weak var favButton: UIButton!
+    @IBOutlet weak var retweetButton: UIButton!
+    @IBOutlet weak var tweetDate: UILabel!
+    
+    var favorited:Bool = false
+    var tweetId: Int = -1
+    
+    func setFavorite(_ isFavorited:Bool){
+        favorited = isFavorited
+        if(favorited){
+            favButton.setImage(UIImage(named:"favor_icon_red"), for: UIControl.State.normal)
+        }else{
+            favButton.setImage(UIImage(named:"favor_icon"), for: UIControl.State.normal)
+        }
+    }
+    
+    @IBAction func favoriteTweet(_ sender: Any) {
+        let tobeFavorited = !favorited
+        if(tobeFavorited){
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(true)
+            }, failure: { (Error) in
+                print("Favorite did not succeed: \(Error)")
+            })
+        }else {
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(false)
+            }, failure: { (Error) in
+                print("Unfavorite did not succeed: \(Error)")
+            })
+        }
+    }
+    
+    @IBAction func retweet(_ sender: Any) {
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+            self.setRetweeted(true)
+        }, failure: { (error) in
+             print("Erroris retweeting: \(error)")
+        })
+    }
+    
+    func setRetweeted(_ isRetweeted: Bool) {
+        if (isRetweeted) {
+            retweetButton.setImage(UIImage(named: "retweet-icon-green"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+        } else {
+            retweetButton.setImage(UIImage(named: "retweet-icon"), for: UIControl.State.normal)
+            retweetButton.isEnabled = true
+        }
+    }
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
